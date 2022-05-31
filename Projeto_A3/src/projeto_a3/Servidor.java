12196d375b4473a9da3495d8ed724edd6c6453a1
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-05-31 08:43:44 -0300

    Servidor/Cliente

--- Projeto_A3/src/projeto_a3/Servidor.java
+++ Projeto_A3/src/projeto_a3/Servidor.java
@@ -5,57 +5,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Servidor implements Runnable{
-  public Socket cliente;
 
-  public void Server(Socket cliente){
-      this.cliente = cliente;
-  }
+public class Servidor {
+    public static void main(String[] args) {
+        try {
+            try (//Criando o servidor 
+            ServerSocket servidor = new ServerSocket(4000)) {
+                System.out.println("Servidor tá on! na porta: " + servidor);
+                //Canal de comunicação
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente do ip" + cliente.getInetAddress().getHostAddress());
 
-  public static void main(String[] args)  throws IOException{     
+                Scanner entrada = new Scanner(cliente.getInputStream());
+                while(entrada.hasNextLine()){
+                    System.out.println(entrada.nextLine());
+                }
 
-      try (//Cria um socket na porta 12345
-      ServerSocket servidor = new ServerSocket (12345)) {
-        System.out.println("Porta 12345 aberta!");
-
-        // Aguarda alguém se conectar. A execução do servidor
-        // fica bloqueada na chamada do método accept da classe
-        // ServerSocket. Quando alguém se conectar ao servidor, o
-        // método desbloqueia e retorna com um objeto da classe
-        // Socket, que é uma porta da comunicação.
-        System.out.println("Aguardando conexão do cliente...");   
-
-        while (true) {
-          Socket cliente = servidor.accept();
-          // Cria uma thread do servidor para tratar a conexão
-          Server tratamento = new Server(cliente);
-          Thread t = new Thread((Runnable) tratamento);
-          // Inicia a thread para o cliente conectado
-          t.start();
+                entrada.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("Erro ao criar o servidor");
         }
-      }
-  }
-
-  /* A classe Thread, que foi instancia no servidor, implementa Runnable.
-     Então você terá que implementar sua lógica de troca de mensagens dentro deste método 'run'.
-  */
-  public void run(){
-      System.out.println("Nova conexao com o cliente " + this.cliente.getInetAddress().getHostAddress());
-
-      try {
-          Scanner s = null;
-          s = new Scanner(this.cliente.getInputStream());
-
-          //Exibe mensagem no console
-          while(s.hasNextLine()){
-              System.out.println(s.nextLine());
-          }
-
-          //Finaliza objetos
-          s.close();
-          this.cliente.close();
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-  }
+    }
+    
 }
