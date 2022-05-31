61ada99912d8be0369c527cb4c157390f393cc85
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-05-31 08:33:11 -0300

    Servidor/Cliente

---
+++ Projeto_A3/src/projeto_a3/Server.java
@@ -0,0 +1,10 @@
+package projeto_a3;
+
+import java.net.Socket;
+
+public class Server {
+
+    public Server(Socket cliente) {
+    }
+
+}
--- Projeto_A3/src/projeto_a3/Servidor.java
+++ Projeto_A3/src/projeto_a3/Servidor.java
@@ -1,34 +1,61 @@
 package projeto_a3;
 
-import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Date;
+import java.util.Scanner;
 
-public class Servidor{
-    public static void main(String[] args) {
-      try {
-        try (// Instancia o ServerSocket ouvindo a porta 1025
-        ServerSocket servidor = new ServerSocket(1025)) {
-            System.out.print("Servidor ouvindo a porta: " + servidor);
-            while(true) {
-              // o método accept() bloqueia a execução até que
-              // o servidor receba um pedido de conexão
-              Socket cliente = servidor.accept();
-              System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
-              ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-              saida.flush();
-              saida.writeObject(new Date(0));
-              saida.close();
-              cliente.close();
-            }
+public class Servidor implements Runnable{
+  public Socket cliente;
+
+  public void Server(Socket cliente){
+      this.cliente = cliente;
+  }
+
+  public static void main(String[] args)  throws IOException{     
+
+      try (//Cria um socket na porta 12345
+      ServerSocket servidor = new ServerSocket (12345)) {
+        System.out.println("Porta 12345 aberta!");
+
+        // Aguarda alguém se conectar. A execução do servidor
+        // fica bloqueada na chamada do método accept da classe
+        // ServerSocket. Quando alguém se conectar ao servidor, o
+        // método desbloqueia e retorna com um objeto da classe
+        // Socket, que é uma porta da comunicação.
+        System.out.println("Aguardando conexão do cliente...");   
+
+        while (true) {
+          Socket cliente = servidor.accept();
+          // Cria uma thread do servidor para tratar a conexão
+          Server tratamento = new Server(cliente);
+          Thread t = new Thread((Runnable) tratamento);
+          // Inicia a thread para o cliente conectado
+          t.start();
         }
       }
-      catch(Exception e) {
-         System.out.println("Erro: " + e.getMessage());
-      }
-      finally {
-        System.out.println("Passou aqui");
-      }
-    }
   }
+
+  /* A classe Thread, que foi instancia no servidor, implementa Runnable.
+     Então você terá que implementar sua lógica de troca de mensagens dentro deste método 'run'.
+  */
+  public void run(){
+      System.out.println("Nova conexao com o cliente " + this.cliente.getInetAddress().getHostAddress());
+
+      try {
+          Scanner s = null;
+          s = new Scanner(this.cliente.getInputStream());
+
+          //Exibe mensagem no console
+          while(s.hasNextLine()){
+              System.out.println(s.nextLine());
+          }
+
+          //Finaliza objetos
+          s.close();
+          this.cliente.close();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+}
